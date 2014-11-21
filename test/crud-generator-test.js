ca4c0ba2d9e385a4860996ca0d93568b3367fcd1
Will McClellan <mcclellan.will@gmail.com>
2014-11-21 12:14:26 +1100

    Tidy up basic tests

--- test/crud-generator-test.js
+++ test/crud-generator-test.js
@@ -2,10 +2,10 @@ var should = require('should');
 
 describe('CrudGenerator: index', function() {
 
-	it('should return a middleware function that returns an array of model resources', function(done) {
-		var CrudGenerator = require('../index');
-		var crud = new CrudGenerator(require('./fixtures/model'))
+	var CrudGenerator = require('../index');
+	var crud = new CrudGenerator(require('./fixtures/model'))
 
+	it('should return a return an array of model resources', function(done) {
 		var allExample = crud.all();
 		allExample.should.have.keys('create', 'destroy', 'index', 'show', 'update');
 
@@ -15,13 +15,29 @@ describe('CrudGenerator: index', function() {
 		var withoutExample = crud.without('update destroy');
 		withoutExample.should.have.keys('create', 'index', 'show');
 
+		done();
+	});
+
+	it('should attach CRUD functions to a controller', function(done) {
 		var userController = {
 			signup: function() {},
 			resetPassword: function() {}
 		};
 		crud.only('index show', userController);
 		userController.should.have.keys('index', 'show', 'signup', 'resetPassword');
+		done();
+	});
 
+	it('should not override any custom CRUD methods already defined on an object', function(done) {
+		var userController = {
+			show: showOverride
+		};
+		crud.without('index destroy', userController);
+		userController.should.have.keys('show', 'create', 'update');
+		userController.show.should.be.exactly(showOverride)
+
+		// our custom show function. Store it so we can test it
+		function showOverride() {};
 		done();
 	});
 
