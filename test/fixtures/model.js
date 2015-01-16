99b0b288afe31f797dd00c0bd5a2a1ff8fb28842
Roland Sharp <rolandnsharp@gmail.com>
2015-01-16 12:33:37 +1100

    Add sorting index tests.

--- test/fixtures/model.js
+++ test/fixtures/model.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
-	description: String
+	description: String,
+	number: Number
 });
 
 module.exports = mongoose.model('example', schema);
---
+++ test/sort-test.js
@@ -0,0 +1,85 @@
+var should = require('should');
+var async = require('async');
+var express = require('express');
+var request = require('supertest');
+require('./lib/mongoose-connection');
+
+var app = express();
+
+describe('CustomError: index', function() {
+    var CrudGenerator = require('../index');
+    var model = require('./fixtures/model');
+    var fixtureData = [];
+
+    var crud = new CrudGenerator(model);
+
+    before(function(done) {
+        model.remove(function() {
+            async.timesSeries(50, createRandomDocument, done)
+        });
+    });
+
+    it('should sort documents in ascending order by number.', function(done) {
+        var handler = crud.index();
+        app.get('/', handler, function(err, req, res, next) {
+            done();
+
+        });
+
+        request(app)
+            .get('/?sort=number')
+            .expect(200, function(err, res) {
+                should.not.exist(err);
+
+                var sorted = res.body.map(function(data){
+                    return data.number;
+                });
+
+                sorted.forEach(function(number, index){
+                    if (index > 1) {
+                        (sorted[index-1] <= number).should.be.true;
+                    }
+                })
+                done();
+            });
+    });
+
+    it.only('should sort documents in descending order by number.', function(done) {
+        var handler = crud.index();
+        app.get('/', handler, function(err, req, res, next) {
+            done();
+
+        });
+
+        request(app)
+            .get('/?sort=-number')
+            .expect(200, function(err, res) {
+                should.not.exist(err);
+
+                var sorted = res.body.map(function(data){
+                    return data.number;
+                });
+
+                sorted.forEach(function(number, index){
+                    if (index > 1) {
+                        (sorted[index-1] >= number).should.be.true;
+                    }
+                })
+                done();
+            });
+    });
+
+    function createRandomDocument(n, callback) {
+        var data = {
+            description: 'nnt, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            number: Math.floor((Math.random() * 99) + 1)
+        };
+        model.create(data, function cb(err, doc) {
+            if (err) { throw err; }
+            fixtureData.push(doc);
+            callback();
+        });
+    }
+
+
+});
